03fa92164fbd9a84ceb8980ba0673a75dcfb2f36
Vinícius Amorim <amorimm.viniciuss@gmail.com>
2020-12-29 17:43:14 -0300

    Environment variables

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-    host: 'smtp.mailtrap.io',
-    port: 2525,
+    host: process.env.MAIL_HOST,
+    port: process.env.MAIL_PORT,
     secure: false,
     auth: {
-        user: '309c79a431c156',
-        pass: '523d2a7a1040e7',
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
     },
     default: {
         from: 'Equipe GoBarber <noreply@gobarber.com>',
--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,4 +1,3 @@
 export default {
-    dsn:
-        'https://7013627d57d6477082c9aa88601084d7@o453197.ingest.sentry.io/5441754',
+    dsn: process.env.SENTRY_DSN,
 };
