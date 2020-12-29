8448ce5e65d16b39a5360d1381dd222d5d76debb
Vinícius Amorim <amorimm.viniciuss@gmail.com>
2020-12-29 11:49:13 -0300

    Configuring NodeMailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+    host: 'smtp.mailtrap.io',
+    port: 2525,
+    secure: false,
+    auth: {
+        user: '309c79a431c156',
+        pass: '523d2a7a1040e7',
+    },
+    default: {
+        from: 'Equipe GoBarber <noreply@gobarber.com>',
+    },
+};
